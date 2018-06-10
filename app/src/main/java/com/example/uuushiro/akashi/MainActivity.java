20f305808d4a6baa6a0b48dfb64f308930c5f9a8
matsutani <yushiro.ma2ta2.21@gmail.com>
2018-06-10 19:26:41 +0900

    MainActivityのonCreate時にインフレーと

--- app/src/main/java/com/example/uuushiro/akashi/MainActivity.java
+++ app/src/main/java/com/example/uuushiro/akashi/MainActivity.java
@@ -7,28 +7,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    Button button_syukkin, button_taikin, button_chokko, button_chokki;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_setting:
-//                    mTextMessage.setText(R.string.title_setting);
-//                    return true;
-//            }
-//            return false;
             displayView(item.getItemId());
             return true;
         }
@@ -38,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
+        displayView(R.id.navigation_home);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
@@ -48,17 +32,14 @@ public class MainActivity extends AppCompatActivity {
     public void displayView(int viewId) {
 
         Fragment fragment = null;
-        String title = getString(R.string.app_name);
 
         switch (viewId) {
             case R.id.navigation_home:
                 fragment = new HomeFragment();
-                title  = "Home";
 
                 break;
             case R.id.navigation_setting:
                 fragment = new SettingFragment();
-                title = "Setting";
                 break;
 
         }
@@ -68,12 +49,6 @@ public class MainActivity extends AppCompatActivity {
             ft.replace(R.id.content_frame, fragment);
             ft.commit();
         }
-
-//        // set the toolbar title
-//        if (getSupportActionBar() != null) {
-//            getSupportActionBar().setTitle(title);
-//        }
-
     }
 
 }
